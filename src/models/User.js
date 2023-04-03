c7a30932056426ac654e6c5eb983db2bcde1c6d3
Pcjmy <1716045191@qq.com>
2023-04-03 22:11:05 +0800

    feat: 设计Schema和Model

---
+++ src/models/User.js
@@ -0,0 +1,14 @@
+const mongoose = require('../db/db')
+
+const Schema = mongoose.Schema({
+  username: {
+    type: String,
+    require: true,
+    unique: true
+  },
+  password: String
+}, { timestamps: true })
+
+const User = mongoose.model('user', Schema)
+
+module.exports = User
