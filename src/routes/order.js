78d042808acc821e275553bc9f391ea98e669d44
Pcjmy <1716045191@qq.com>
2023-04-12 14:53:48 +0800

    feat: order router

---
+++ src/routes/order.js
@@ -0,0 +1,18 @@
+const router = require('koa-router')()
+
+const { SuccessModel, ErrorModel } = require('../res-module/index')
+const loginCheck = require('../middleware/loginCheck')
+
+router.prefix('/api/order')
+
+// 创建订单
+router.post('/', loginCheck, async function(ctx, next) {
+    // 当前用户名
+    const userInfo = ctx.session.userInfo
+    const username = userInfo.username
+
+    // 获取订单数据
+    const data = ctx.request.body
+})
+
+module.exports = router
