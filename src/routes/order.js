dc47600d293edb808fcd8515d0f2abb5ef0e2263
Pcjmy <1716045191@qq.com>
2023-04-13 10:19:35 +0800

    feat: 创建订单

--- src/routes/order.js
+++ src/routes/order.js
@@ -13,6 +13,14 @@ router.post('/', loginCheck, async function(ctx, next) {
 
     // 获取订单数据
     const data = ctx.request.body
+
+    //创建订单
+    try {
+        const newOrder = await createOrder(username, data)
+        ctx.body = new SuccessModel(newOrder)
+    } catch (ex) {
+       
+    }
 })
 
 module.exports = router
