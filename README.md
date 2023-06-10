f2bdf4578a6b24456df8290104115781a1617fe9
Pcjmy <1716045191@qq.com>
2023-06-10 21:09:45 +0800

    feat: 获取用户信息

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# README
+## README
 
-## 项目搭建
+### 项目搭建
 
 ```shell
 > npm i koa-generator -g
@@ -10,13 +10,13 @@
 > npm run dev
 ```
 
-## 环境信息
+### 环境信息
 
 node v14.15.1
 
 npm 6.14.8
 
-## 安装工具
+### 安装工具
 
 ```shell
 > npm i koa-generic-session --save
--- src/routes/users.js
+++ src/routes/users.js
@@ -1,6 +1,7 @@
 const router = require('koa-router')()
 const { register, login } = require('../controller/user')
 const { SuccessModel, ErrorModel } = require('../res-model/index')
+const loginCheck = require('../middleware/loginCheck')
 
 router.prefix('/api/user')
 
@@ -26,6 +27,11 @@ router.post('/login', async function(ctx, next) {
   } else {
     ctx.body = new ErrorModel(10002, '登录验证失败')
   }
+})
+
+router.get('/info', loginCheck, async function(ctx, next) {
+  const session = ctx.session
+  ctx.body = new SuccessModel(session.userInfo)
 })
 
 module.exports = router
